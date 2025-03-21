d30b71fe938c87bfaa2f334b0185debed6956d3e
강소현 <conut1119@gmail.com>
2025-03-21 19:25:06 +0900

    [level 0] Title: 홀짝 구분하기, Time: 23.10 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        int n = int.Parse(s);
+        
+        
+        if(n % 2 == 0)
+        {
+            Console.WriteLine($"{n} is even");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is odd");
+        }
+
+    
+    }
+}
