af4416106cd26dab55d9bb4b506dd08fc039cfdd
강소현 <conut1119@gmail.com>
2025-03-30 23:56:32 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.66 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        string a = n.ToString();
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            answer += n % 10;
+            n = n / 10;
+        }
+        return answer;
+     }
+}
