aa7c3729aa4949504c7878f22f6b011d5c36ac75
강소현 <conut1119@gmail.com>
2025-04-19 21:07:14 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 22.92 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input;
+        string answer = "";
+
+        Console.Clear();
+        input = Console.ReadLine().Split(' ');
+
+        String s1 = input[0];
+        int a = Int32.Parse(input[1]);
+        
+        for(int i = 0; i < a;i++)
+        {
+            answer += s1;
+        }
+        Console.WriteLine(answer);
+
+    }
+}
