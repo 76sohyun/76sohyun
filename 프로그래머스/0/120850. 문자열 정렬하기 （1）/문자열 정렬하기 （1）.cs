f04f8f8bf2de44a3bfed10a1e1590d0a3d1be952
강소현 <conut1119@gmail.com>
2025-04-20 23:32:39 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 41.53 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.RegularExpressions;
+public class Solution {
+    public int[] solution(string my_string) {
+        string str = Regex.Replace(my_string, @"\D","");
+        string[] str2 = new string[str.Length];
+        int[] answer = new int[str.Length];
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            str2[i] = str[i].ToString();
+        }
+        
+        Array.Sort(str2);
+
+        for (int i = 0; i < str2.Length; i++)
+        {
+            answer[i] = Int32.Parse(str2[i]);
+        }
+        return answer;
+    }
+}
