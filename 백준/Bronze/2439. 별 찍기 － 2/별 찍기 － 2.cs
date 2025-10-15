8966c4cc081723cb0e3524c269c81ade84b5aded
강소현 <conut1119@gmail.com>
2025-10-16 01:06:38 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 332 ms, Memory: 6584 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int N);
+
+            for (int i = 0; i < N; i++)
+            {
+                int mul = i + 1;
+                int min = N - mul;
+                
+                for (int l = 0; l < min; l++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int j = 0; j < mul; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
