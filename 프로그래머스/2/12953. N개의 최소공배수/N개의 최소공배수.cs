75f124a24fcab13d4eb4dfa4d585e9f37b898c70
강소현 <conut1119@gmail.com>
2026-01-09 18:10:42 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        
+        for(int i = 0; i < arr.Length; i++)
+        {
+            lcm = LCM(lcm, arr[i]);
+        }
+        return lcm;
+    }
+    
+    private int GCD(int a, int b)
+    {
+        while(b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    private int LCM(int a, int b)
+    {
+        return a * b / GCD(a,b);
+    }
+}
